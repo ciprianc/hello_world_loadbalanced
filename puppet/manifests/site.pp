3e37a4571bb48250865b3033d30b4c29bcc8cd66
Ciprian Ciubotariu <ciprian.ciubotariu@digital.hmrc.gov.uk>
2015-02-19 19:49:32 +0000

    forgot about the sudo requirement

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,21 +1,18 @@
 node loadbalancer {
-  include baseconfig
+  include sudo
   include nginx
 
-  nginx::resource::vhost { '_':
-    proxy => 'http://ciprians_app',
-  }
+  sudo::conf { 'vagrant': priority => 10 , content  => "vagrant ALL=(ALL) NOPASSWD: ALL" }
+  sudo::conf { 'admins': priority => 10, content  => "%admins ALL=(ALL) NOPASSWD: ALL" }
 
-  nginx::resource::upstream { 'ciprians_app':
-    members => [
-     '192.168.50.2:8080',
-     '192.168.50.3:8080',
-    ],
+  nginx::resource::vhost { '_': proxy => 'http://ciprians_app', }
+  nginx::resource::upstream { 'ciprians_app': 
+    members => [ '192.168.50.2:8080', '192.168.50.3:8080', ],
   }
 }
 
 node /web-[0-9]/ {
   include super_simple_app
-  include baseconfig
 }
+
 
